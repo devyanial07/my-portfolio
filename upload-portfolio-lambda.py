91769635e89ecad40673d6900427dcf0d7b98602
George Sheaffer <gsheaf@gmail.com>
2017-11-29 12:06:10 -0500

    Change file type mimetype

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -37,8 +37,8 @@ def lambda_handler(event, context):
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for name in myzip.namelist():
                 obj = myzip.open(name)
-                portfolio_bucket.upload_fileobj(obj, name)
-                ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]}
+                portfolio_bucket.upload_fileobj(obj, name,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
                 portfolio_bucket.Object(name).Acl().put(ACL='public-read')
 
 
